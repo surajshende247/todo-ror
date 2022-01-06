2dc4a1d72024fef153e331ef21f09b6f154caf17
Suraj Shende <surajshende247@gmail.com>
2022-01-06 14:52:34 +0530

    root route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :lists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  root 'lists#index'
 end
